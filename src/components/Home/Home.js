991dd0ac474c71487082347bcfe59755ea69a3dd
alalUDDIN123 <alalu75666@gmail.com>
2023-10-26 09:32:22 +0530

    add:helmet

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,29 +7,41 @@ import { Projects } from "../projects/Projects";
 import ContactMe from "../contact/ContactMe";
 import HeroSection from "./HeroSection";
 import About from "../About/About";
+import { Helmet } from "react-helmet";
+
 function Home() {
   return (
-    <section>
-      <Container fluid className="home-section" id="home">
-        <Particle />
-        <HeroSection />
-      </Container>
 
-      {/* about */}
 
-      <About />
+    <>
 
-      {/* skilss section */}
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      <section>
+        <Container fluid className="home-section" id="home">
+          <Particle />
+          <HeroSection />
+        </Container>
 
-      <Skills />
+        {/* about */}
 
-      {/* projects */}
-      <Projects />
+        <About />
 
-      {/* contact section */}
+        {/* skilss section */}
 
-      <ContactMe />
-    </section>
+        <Skills />
+
+        {/* projects */}
+        <Projects />
+
+        {/* contact section */}
+
+        <ContactMe />
+      </section>
+    </>
   );
 }
 
